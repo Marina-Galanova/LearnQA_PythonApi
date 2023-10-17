62b1e1bab283c1899cd5095ce92316b08e6b280d
Галанова Марина <mmgalanova@fintechiq.ru>
2023-10-17 15:41:28 +0700

    long_redirect

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello from Marina")
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+history_response = response.history
+final_response = response
+
+print(history_response)
+print(final_response.url)
