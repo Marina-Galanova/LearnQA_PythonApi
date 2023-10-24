feb03bc5afb6b4facc75449acf6a1175d964fb0f
Галанова Марина <mmgalanova@fintechiq.ru>
2023-10-24 13:06:06 +0700

    EX11. add file test_api_cookie.py

---
+++ test_api_header.py
@@ -0,0 +1,10 @@
+import requests
+
+class TestApiHeader:
+    def test_chek_header(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_header")
+
+        assert response.status_code == 200, f"Некоррекный статус код"
+
+        response_header = response.headers
+        assert "x-secret-homework-header" in response_header, f"Некорректный хедер"
